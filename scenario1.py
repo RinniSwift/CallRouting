3ca95284bc0444a3bd29ef889de735bccc2e4ff7
Rinni Swift <rinradaswift@gmail.com>
2019-05-09 14:57:36 -0700

    complete search for words 100 words

--- scenario1.py
+++ scenario1.py
@@ -1,21 +1,38 @@
 
 class SolutionOne(object):
 
-	def create_number_dictionary(self, file):
+	def __init__(self, file_name):
+		self.file_name = file_name
+
+	def create_number_dictionary(self):
 
 		route_costs_dict = {}
 
-		with open(file) as txt_file:
+		with open(self.file_name) as txt_file:
 			for line in txt_file:
 				arr_per_line = line.split(",")
 				route_costs_dict[arr_per_line[0]] = arr_per_line[1][:-1]
 
-		print(route_costs_dict)
+		return route_costs_dict
+
+	def search_number(self, number):
+		all_route_costs = self.create_number_dictionary()
+
+		while len(number) > 1:
+
+			try:
+				if all_route_costs[number] is not None:
+					return all_route_costs[number]
+			except:
+				number = number[:-1]
+
+		return None
 
 
 def main():
-	solution = SolutionOne()
-	solution.create_number_dictionary("route-costs-100.txt")
+	solution = SolutionOne("route-costs-100.txt")
+
+	print(solution.search_number("+44918964408384"))
 
 if __name__ == "__main__":
 	main()
