083367da95422c3441af35b6b763a6af1e6596d0
Medi Assumani <mediassumani49@gmail.com>
2019-05-14 18:48:47 -0700

    stuff

--- scenario3.py
+++ scenario3.py
@@ -8,35 +8,34 @@ class SolutionOne(object):
 
 		self.phone_file = phone_file
 		self.carrier_file_array = carrier_file_array
+		self.all_route_dict = {}
 
-
-	def create_route_dictionary(self, carrier_file):
+	def create_route_dictionary(self):
 		''' create a key-value pair for each route and its cost
 
 			Time Complexity : O(n * m) where n is the number of lines, m is the length of each line 
 			Space Complexity : O(n) where n is the size of the dictionary
 		'''
-		route_costs_dict = {}  # O(1) time | O(1) space
+		#route_costs_dict = {}  # O(1) time | O(1) space
 
-		try:
-			with open(carrier_file) as txt_file: # O(1) time | O(1) space
+		for file_name in self.carrier_file_array:
 
-				for line in txt_file: # O(n) time 
+			try:
+				with open(file_name) as txt_file: # O(1) time | O(1) space
+					for line in txt_file: # O(n) time 
 
-					arr_per_line = line.split(",") # O(n) time | O(n) space
-					if arr_per_line[0] in route_costs_dict:
-						
-						route_costs_dict[arr_per_line[0]].append(arr_per_line[1][:-1]) # O(1) time | O(1) space
-					else:
-						route_costs_dict[arr_per_line[0]] = [arr_per_line[1][:-1]] # O(1) time | O(1) space
-						
-		except IOError as error:
-			print("error {} found while opening file {}".format(error, carrier_file))
-
-		return route_costs_dict
+						arr_per_line = line.split(",") # O(n) time | O(n) space
+						if arr_per_line[0] in self.all_route_dict:
+							self.all_route_dict[arr_per_line[0]].append(arr_per_line[1][:-1]) # O(1) time | O(1) space
+						else:
+							#print(arr_per_line[1])
+							self.all_route_dict[arr_per_line[0]] = [arr_per_line[1][:-1]] # O(1) time | O(1) space
+							
+			except IOError as error:
+				print("error {} found while opening file {}".format(error, file_name))
 
 
-	def find_cost(self, number, route_dict):
+	def find_cost(self, number):
 		''' look up number in dictionary of route-cost.
 
 			Time Complexity : O(n) worst case where n is the length of the number | O(1) best case if initial number is the route
@@ -44,8 +43,8 @@ class SolutionOne(object):
 		'''
 		while len(number) > 1: # O(n-1) time worst case | O(1) time best case if initial number is the route
 			try: 
-				if route_dict[number] is not None: #O(1) time 
-					costs = route_dict[number]
+				if self.all_route_dict[number] is not None: #O(1) time 
+					costs = self.all_route_dict[number]
 					return min(costs)
 					
 			except:
@@ -70,6 +69,7 @@ class SolutionOne(object):
 		
 		return phone_num_arr
 
+	
 	def create_route_numbers_dict(self):
 
 		numbers = self.create_number_array()
@@ -82,12 +82,12 @@ class SolutionOne(object):
 					
 					current_carrier_dict = self.create_route_dictionary(carrier_file)
 					curr_min_cost = self.find_cost(num, current_carrier_dict)
+
 					if curr_min_cost:
 						minimum_costs.append(curr_min_cost)
 
 				if len(minimum_costs) > 1:
-					# num_cost_dict[num] = min(minimum_costs)
-					print(minimum_costs)
+					num_cost_dict[num] = min(minimum_costs)
 					
 					
 		return num_cost_dict
@@ -95,7 +95,7 @@ class SolutionOne(object):
 	
 def main():
 	
-	arr_of_carriers = ["dummy_1.txt", "dummy_2.txt"]
+	arr_of_carriers = ["route-costs-10.txt", "route-costs-35000.txt", "route-costs-106000.txt", "route-costs-1000000.txt", "route-costs-10000000.txt"]
 	solution = SolutionOne("phone-numbers-10.txt", arr_of_carriers)
 
 	print(solution.create_route_numbers_dict())
